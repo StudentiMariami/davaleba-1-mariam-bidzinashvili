24fbf3e83d0a8622abfbef7dd402587831a88dd5
Mariami <Mariami@MARIAMI_ASUS>
2023-10-03 23:21:38 +0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            matarebeli sadguri= new matarebeli();
+            int vagonebis_raod, vagonshi_mgzavr=35;
+            sadguri.biletebis_fasi = int.Parse(textBox1.Text);
+            sadguri.biletebis_raod = int.Parse(textBox2.Text);
+
+      /*      public void vagonebi(double vagonebi)
+            {
+                vagonebis_raod = (int)vagonebi;
+                double mag = biletebis_raod / vagonshi_mgzavr;
+                if (biletebis_raod % vagonshi_mgzavr == 0)
+                {
+                    vagonebi = mag;
+                }
+                else
+                {
+                    vagonebi = Math.Ceiling(mag);
+                }
+            }*/
+
+            sadguri.mogeba(label1);
+            sadguri.gamotana(label2);
+
+
+        }
+    }
+}
---
+++ matarebeli.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class matarebeli
+    {
+        private int vagonebis_raod, vagonshi_mgzavr=35,mog;
+        public int biletebis_fasi, biletebis_raod;
+        public void gamotana(System.Windows.Forms.Label lab2)
+        {
+            if (biletebis_raod % vagonshi_mgzavr == 0)
+            {
+                vagonebis_raod = biletebis_raod / vagonshi_mgzavr;
+            }
+            else
+            {
+                vagonebis_raod=(biletebis_raod/vagonshi_mgzavr)+1;
+            }
+    
+            lab2.Text="sul aris"+vagonebis_raod.ToString()+" vagoni"+
+                "\n1 vagonshi eteva "+vagonshi_mgzavr.ToString()+"mgzavri+" +
+                "\nbiletis fasia "+biletebis_fasi.ToString()+
+                "\ngakiduli biletebis rodenobaa "+biletebis_raod.ToString();
+        }
+        public void mogeba(System.Windows.Forms.Label lab1)
+        {
+            mog = biletebis_raod * biletebis_fasi;
+            lab1.Text = "sul shemosuli tanxaa: " + mog.ToString();
+        }
+    }
+}
